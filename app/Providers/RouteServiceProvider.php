8315a827db3171a0d72f13383560eed71f7c0a9a
PrasastyDaraDDS <daraprasasty075@gmail.com>
2023-04-11 13:45:33 +0700

    mengubah redirect login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/roti';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
